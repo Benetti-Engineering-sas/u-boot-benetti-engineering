8297fe7ad248db3f7e33f61837b0399f7adc3998
Giulio Benetti <giulio.benetti@benettiengineering.com>
2021-04-13 22:46:59 +0200

    ARM: IMXRT: add support for FEC_MXC
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>

--- drivers/net/fec_mxc.c
+++ drivers/net/fec_mxc.c
@@ -593,7 +593,7 @@ static int fecmxc_init(struct udevice *dev)
 
 	/* Do not access reserved register */
 	if (!is_mx6ul() && !is_mx6ull() && !is_imx8() && !is_imx8m() && !is_imx8ulp() &&
-	    !is_imx93()) {
+	    !is_imx93() && !is_imxrt()) {
 		/* clear MIB RAM */
 		for (i = mib_ptr; i <= mib_ptr + 0xfc; i += 4)
 			writel(0, i);
@@ -1488,6 +1488,7 @@ static const struct udevice_id fecmxc_ids[] = {
 	{ .compatible = "fsl,imx6ul-fec" },
 	{ .compatible = "fsl,imx53-fec" },
 	{ .compatible = "fsl,imx7d-fec" },
+	{ .compatible = "fsl,imxrt-fec" },
 	{ .compatible = "fsl,mvf600-fec" },
 	{ .compatible = "fsl,imx93-fec" },
 	{ }
