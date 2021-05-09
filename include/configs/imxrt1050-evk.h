8e8705247f5a23ae6875f21b830f26f51cd8dd33
Giulio Benetti <giulio.benetti@benettiengineering.com>
2021-05-10 00:34:22 +0200

    TEST configs: imxrt1050-evk: set a MAC address and an ip address to test
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>

--- include/configs/imxrt1050-evk.h
+++ include/configs/imxrt1050-evk.h
@@ -24,5 +24,13 @@
 
 #define CFG_SYS_UBOOT_BASE			0x60010000
 
+#ifdef CONFIG_VIDEO
+#define CFG_EXTRA_ENV_SETTINGS \
+		"stdin=serial\0" \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0" \
+		"ethaddr=00:0A:F7:95:65:A4\0" \
+		"ipaddr=192.168.50.240\0"
+#endif
 
 #endif /* __IMXRT1050_EVK_H */
